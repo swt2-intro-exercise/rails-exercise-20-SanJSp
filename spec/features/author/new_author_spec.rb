5ca0b4acd8af6a6788fec45b9536c58d68266d51
Sandro <sandro.speh@student.hpi.de>
2020-11-10 12:16:07 +0100

    created test for author page

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,10 @@ describe "New author page", type: :feature do
  it "should render withour error" do
    visit new_author_path
  end
+ it 'should have a first name, last name and homepage input field' do
+   visit new_author_path
+   expect(page).to have_field('author[first_name]')
+   expect(page).to have_field('author[last_name]')
+   expect(page).to have_field('author[homepage]')
+ end
 end
