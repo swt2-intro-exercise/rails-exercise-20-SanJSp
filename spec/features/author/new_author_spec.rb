d47a2cd1cc2c5d7d147aeb952128431cd1607cfd
sandro <sandro.speh@student.hpi.de>
2020-11-20 17:30:38 +0100

    created test for #7

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -15,6 +15,13 @@ describe "New author page", type: :feature do
  it 'should not be possible to create author without lastname' do
    @author = Author.new(first_name:'Rosa',last_name: nil, homepage:'meineschluepfer.com')
    expect(@author).to_not be_valid
-
  end
+
+ it 'should show error when lastname is not filled and submit is clicked' do
+   visit new_author_path
+   #@author = Author.new(first_name:'Rosa', last_name: nil, homepage:'meineschluepfer.com')
+   author = Author.create({"first_name" => "Alan", "homepage" => "http://wikipedia.org/Alan_Turing"})
+   expect(author.errors.full_messages.each).to include("Last name can't be blank")
+ end
+
 end
