1be91c44d65094e07323573761e496150b02101a
sandro <sandro.speh@student.hpi.de>
2020-11-20 16:31:08 +0100

    implemented author index page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,12 @@
 class AuthorsController < ApplicationController
+  def index
+    @author = Author.all
+  end
+
   def show
     @author = Author.find(params[:id])
   end
+
 
   def new
     @author = Author.new
