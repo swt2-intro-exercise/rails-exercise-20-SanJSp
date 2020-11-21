a921641c7b9b055deb627753d16057669b6b0c8f
sandro <sandro.speh@student.hpi.de>
2020-11-21 12:11:28 +0100

    added test for #12

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,4 +5,9 @@ describe "New paper page", type: :feature do
     @paper = Paper.new(venue:"HPI", year:2020)
     expect(@paper).to_not be_valid
   end
+
+  it 'should not be possible to create a paper without a venue' do
+    @paper = Paper.new(title:"Introduction to Deep Learning", year:2020)
+    expect(@paper).to_not be_valid
+  end
 end
