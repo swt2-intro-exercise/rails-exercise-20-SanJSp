1d9d19165d256e83e77d6cae11904fb517a7e21d
sandro <sandro.speh@student.hpi.de>
2020-11-21 11:31:42 +0100

    created test for #10

---
+++ spec/models/paper_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Paper, type: :model do
+  it 'should have a title and venue string as well as a year integer' do
+    @paper = FactoryBot.create :paper
+    expect(@paper.title).to eq("COMPUTING MACHINERY AND INTELLIGENCE")
+    expect(@paper.venue).to eq("Mind 49: 433-460")
+    expect(@paper.year).to eq(1950)
+    end
+end
