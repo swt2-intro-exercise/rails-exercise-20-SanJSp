7620ed469f217ba113737c46c114dc281b88a89c
sandro <sandro.speh@student.hpi.de>
2020-11-21 12:26:57 +0100

    added test for #14

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -15,4 +15,10 @@ describe "New paper page", type: :feature do
     @paper = Paper.new(title:"Introduction to Deep Learning", venue:"HPI")
     expect(@paper).to_not be_valid
   end
+
+  it 'should not be possible to create a paper with an non-integer year' do
+    @paper = Paper.new(title:"Introduction to Deep Learning", venue:"HPI", year:"twothaushendtwenty")
+    expect(@paper).to_not be_valid
+  end
+
 end
